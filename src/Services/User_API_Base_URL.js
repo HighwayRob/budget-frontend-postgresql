f897b7f25699498a827a9ac920f31bffcac3324b
HighwayRob <robsobol@yahoo.com>
2022-03-24 15:19:14 -0400

    Changes-032422

--- src/Services/User_API_Base_URL.js
+++ src/Services/User_API_Base_URL.js
@@ -1,3 +1,6 @@
 //export const USER_API_BASE_URL = "http://localhost:8080/api/v1/";
 
-export const USER_API_BASE_URL = "http://rmsystem.ddns.net:8080/api/v1/";
+//export const USER_API_BASE_URL = "http://rmsystem.ddns.net:8080/api/v1/";
+
+export const USER_API_BASE_URL = 
+"postgres://xmmazemfflgbkj:a323bc337c559fffd0ec7029daaf60f03712cdb1df41ca8629fe8429f0a7a607@ec2-54-85-113-73.compute-1.amazonaws.com:5432/d423nfcf5dbjf7";
